ce7386e7a619d0a74309a6ba94754beb08a4ea42
mahmoud-wahba-dev <mahmoud.wahba.dev@gmail.com>
2023-04-02 18:12:03 +0200

    add type sccript intro

---
+++ index.js
@@ -0,0 +1,10 @@
+// typescript features  : prdictable code 
+// reduce errors coz errors occurs at develpoment time not run time or after running project
+// npm install typescript -g 
+// will install tsc => type script compiler 
+// so when u write tsc index.ts will translate type script to js code 
+// tsc indec.ts --watch
+(function () {
+    var userName = "ahmed";
+    var userAge = 20;
+})();
---
+++ index.ts
@@ -0,0 +1,15 @@
+// typescript features  : prdictable code 
+// reduce errors coz errors occurs at develpoment time not run time or after running project
+// npm install typescript -g 
+// will install tsc => type script compiler 
+// so when u write tsc index.ts will translate type script to js code 
+// tsc indec.ts --watch
+ 
+
+
+
+
+(function () {
+let userName:string = "ahmed"
+let userAge:number = 20;
+}) ()
